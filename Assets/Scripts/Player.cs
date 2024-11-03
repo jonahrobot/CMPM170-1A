624b54be34a72249c56a07a28edcb77ee0872013
Jonah Ryan <jonahrobot@gmail.com>
2024-11-02 21:49:11 -0700

    Pulse every beat, different beats

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(Attachable))]
 public class Player : MonoBehaviour
 {
-    public float speed = 1.0f;
+    public float movementSpeed = 1.0f;
+    public float beatRateInSeconds = 1.0f;
+
+    int beatTrack = 1;
 
     private Attachable attachable;
 
     void Start()
     {
         attachable = GetComponent<Attachable>();
+        StartCoroutine("beat");
     }
 
     void Update()
@@ -26,6 +30,34 @@ public class Player : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
-        transform.Translate(movement * speed * Time.deltaTime);
+        transform.Translate(movement * movementSpeed * Time.deltaTime);
+
+        if(transform.localScale.x > 1 && transform.localScale.y > 1)
+        {
+            transform.localScale = new Vector3(transform.localScale.x - 1f * Time.deltaTime, transform.localScale.y - 1f * Time.deltaTime, 1);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+        }
+    }
+
+    IEnumerator beat()
+    {
+        yield return new WaitForSeconds(beatRateInSeconds);
+
+        // Play sound main
+        attachable.Step(1);
+
+        // Play every other beat
+        if(beatTrack % 2 == 0) attachable.Step(2);
+
+        // Play every fourth beat
+        if (beatTrack % 4 == 0) attachable.Step(0);
+
+        beatTrack += 1;
+        transform.localScale = new Vector3(1.5f, 1.5f, 1);
+
+        StartCoroutine("beat");
     }
 }
